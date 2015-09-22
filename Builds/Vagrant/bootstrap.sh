3d8d93c1e5414d092e8e81bc2d64837dd52848e8
crazyquark <sam.bitcoin@gmail.com>
2015-09-22 14:44:12 +0300

    vagrant overhaul; dump inline scripts

--- Builds/Vagrant/bootstrap.sh
+++ Builds/Vagrant/bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install -y git scons pkg-config protobuf-compiler libprotobuf-dev libssl
 apt-get install -y python-software-properties
 apt-get install -y build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev
 
+# install boost
 pushd /src/
 if [ ! -d "boost_1_58_0" ]; then
   if [ ! -f "boost_1_58_0.tar.gz" ]; then
@@ -13,28 +14,11 @@ if [ ! -d "boost_1_58_0" ]; then
   fi
   tar -vxf boost_1_58_0.tar.gz
 fi
+
 pushd boost_1_58_0
 . ./bootstrap.sh
 ./b2 install
 ldconfig
 popd
 
-scons release
-(service rippled stop &) || true
-cp /src/build/gcc.release/rippled /usr/local/sbin/rippled && strip /usr/local/sbin/rippled
 popd
-
-# configs
-mkdir -p /etc/ripple/
-mkdir -p /root/.config/ripple/
-mkdir -p /home/vagrant/.config/ripple/
-cp /src/Builds/Vagrant/validators.txt /etc/ripple/validators.txt
-
-# service
-cp /src/Builds/Vagrant/init.d_rippled /etc/init.d/rippled
-chmod +x /etc/init.d/rippled
-cp /src/Builds/Vagrant/init.d_rippled2 /etc/init.d/rippled2
-chmod +x /etc/init.d/rippled2
-flip -u /etc/init.d/rippled
-flip -u /etc/init.d/rippled2
-flip -u /etc/ripple/validators.txt
---
+++ Builds/Vagrant/bootstrap_and_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# build ripple
+pushd /src/
+scons release
+(service rippled stop &) || true
+cp /src/build/gcc.release/rippled /usr/local/sbin/rippled && strip /usr/local/sbin/rippled
+popd
+
+# configs
+mkdir -p /etc/ripple/
+mkdir -p /root/.config/ripple/
+mkdir -p /home/vagrant/.config/ripple/
+cp /src/Builds/Vagrant/validators.txt /etc/ripple/validators.txt
+
+# service
+cp /src/Builds/Vagrant/init.d_rippled /etc/init.d/rippled
+chmod +x /etc/init.d/rippled
+cp /src/Builds/Vagrant/init.d_rippled2 /etc/init.d/rippled2
+chmod +x /etc/init.d/rippled2
+flip -u /etc/init.d/rippled
+flip -u /etc/init.d/rippled2
+flip -u /etc/ripple/validators.txt
+
+# start service
+cp /src/Builds/Vagrant/rippled_one.cfg /etc/ripple/rippled.cfg
+cp /src/Builds/Vagrant/rippled_one.cfg /root/.config/ripple/rippled.cfg
+cp /src/Builds/Vagrant/rippled_one.cfg /home/vagrant/.config/ripple/rippled.cfg
+flip -u /etc/ripple/rippled.cfg
+service rippled start
