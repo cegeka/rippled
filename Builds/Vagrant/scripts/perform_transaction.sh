acf561049a3cc2c603b97dab8c3816eb4a4262c4
Andrei Grigoriu <mudpedal@vertexarmy.org>
2015-10-01 12:53:55 +0300

    Added a small stress script to test fee 0 persistence

---
+++ Builds/Vagrant/scripts/perform_transaction.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# params : account_src secret account_dst [address] [port]
+
+ADDRESS="localhost"
+PORT="5105"
+
+if ! [ -z $4 ]
+	then
+		ADDRESS="$4"
+fi
+
+if ! [-z $5 ]
+	then
+		PORT="$5"
+fi
+
+if [ "$#" -lt 3 ]
+	then
+		echo "Invalid params, need 3 params: account_src secret account_dst";
+		exit 1;
+fi
+
+echo "ADDRESS: $ADDRESS:$PORT";
+
+ACCOUNT_SRC="$1"
+SECRET="$2"
+ACCOUNT_DST="$3"
+
+command -v js24 > /dev/null || sudo apt-get install libmozjs-24-bin  
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "'"$SECRET"'", "tx_json": { "Account": "'"$ACCOUNT_SRC"'" , "Amount": "20000000", "Destination": "'"$ACCOUNT_DST"'", "DestinationTag": "1", "TransactionType": "Payment" } } ] }' http://$ADDRESS:$PORT/`
+tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
+
+echo
+echo  "tx_blob is: $tx_blob\n"
+echo
+
+curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" } ] }' http://$ADDRESS:$PORT/
+
+
---
+++ Builds/Vagrant/scripts/stress_fee.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ACCOUNT1="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+SECRET1="masterpassphrase"
+ACCOUNT2="r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg"
+SECRET2="shrixAzTwHCDGfYTMUFcqhwdWVzCp"
+ADDRESS="10.162.128.122"
+PORT="5105"
+
+result1=`./perform_transaction.sh $ACCOUNT1 $SECRET1 $ACCOUNT2 $ADDRESS $PORT`
+echo "$result1"
+result2=`./perform_transaction.sh $ACCOUNT2 $SECRET2 $ACCOUNT1 $ADDRESS $PORT`
+echo "$result2"
+
+
+
+
