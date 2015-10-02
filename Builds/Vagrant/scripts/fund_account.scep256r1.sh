9091d6b2defe08e29f08f9b64fd1c59ab1ab3614
crazyquark <sam.bitcoin@gmail.com>
2015-10-02 17:00:34 +0300

    new fund account script

---
+++ Builds/Vagrant/scripts/fund_account.scep256r1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ACCOUNT="$1"
+if [ -z $ACCOUNT ]
+        then
+                ACCOUNT="rJYTxryj4ZudrvvxXk5igGGN7UgZ3tbpzb"
+fi
+PORT="$2"
+if [ -z $PORT ]
+	then
+		PORT=5105
+fi
+
+command -v js24 > /dev/null || sudo apt-get install libmozjs-24-bin  
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "masterpassphrase", "tx_json": { "Account": "rfWpbDgyaFPLkTDNKmF13zkvuy62NPQ8Ku" , "Amount": "999000000", "Destination": "'"$ACCOUNT"'", "DestinationTag": "1", "TransactionType": "Payment" } } ] }' http://localhost:$PORT/`
+tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
+
+echo
+echo  "tx_blob is: $tx_blob\n"
+echo
+
+curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" } ] }' http://localhost:$PORT/
+
+
+echo "Funding with EUR"
+result=`curl -X POST -d '{ "method": "sign", "params": [ { "offline": false, "secret": "shrixAzTwHCDGfYTMUFcqhwdWVzCp", "tx_json": { "Account": "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "Amount": { "currency": "EUR", "issuer" : "r4gzWvzzJS2xLuga9bBc3XmzRMPH3VvxXg", "value": "150" }, "Destination": "'"$ACCOUNT"'", "DestinationTag": "1", "TransactionType": "Payment" } } ] }' http://localhost:5105/`
+
+tx_blob=`echo $result | ./jsawk -j js24 'return this.result.tx_blob'`
+
+curl -X POST -d '{ "method": "submit", "params": [ { "tx_blob": "'"$tx_blob"'" } ] }' http://localhost:$PORT/
