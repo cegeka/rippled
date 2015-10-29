3de51673041941b68c79338c09503f92f0b29167
crazyquark <sam.bitcoin@gmail.com>
2015-10-29 12:38:23 +0200

    inplace setup script

---
+++ Builds/Vagrant/inplace_setup_ripple.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# rippled
+cp ../../src/build/gcc.release/rippled /usr/local/sbin/rippled
+
+# configs
+sudo mkdir -p /etc/ripple/
+sudo chown -R $USER /etc/ripple
+mkdir -p $HOME/.config/ripple/
+
+cp validators.txt /etc/ripple/validators.txt
+
+# service
+if [ "$1" -eq "four" ]; then
+	cp init.d_rippled_four /etc/init.d/rippled
+else
+	cp init.d_rippled /etc/init.d/rippled
+fi
+
+chmod +x /etc/init.d/rippled
+cp init.d_rippled2 /etc/init.d/rippled2
+
+chmod +x /etc/init.d/rippled2
+flip -u /etc/init.d/rippled
+flip -u /etc/init.d/rippled2
+flip -u /etc/ripple/validators.txt
+
+# start service
+cp rippled_$1.cfg /etc/ripple/rippled.cfg
+cp rippled_$1.cfg $HOME/.config/ripple/rippled.cfg
+flip -u /etc/ripple/rippled.cfg
+sudo service rippled start
