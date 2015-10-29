61453aa8a1316cff9c4b78b68003be9c43f52b61
crazyquark <sam.bitcoin@gmail.com>
2015-10-29 12:44:16 +0200

    more permissions

--- Builds/Vagrant/inplace_setup_ripple.sh
+++ Builds/Vagrant/inplace_setup_ripple.sh
@@ -13,21 +13,21 @@ cp validators.txt /etc/ripple/validators.txt
 
 # service
 if [ "$1" -eq "four" ]; then
-	cp init.d_rippled_four /etc/init.d/rippled
+	sudo cp init.d_rippled_four /etc/init.d/rippled
 else
-	cp init.d_rippled /etc/init.d/rippled
+	sudo cp init.d_rippled /etc/init.d/rippled
 fi
 
-chmod +x /etc/init.d/rippled
-cp init.d_rippled2 /etc/init.d/rippled2
+sudo chmod +x /etc/init.d/rippled
+sudo cp init.d_rippled2 /etc/init.d/rippled2
 
-chmod +x /etc/init.d/rippled2
-flip -u /etc/init.d/rippled
-flip -u /etc/init.d/rippled2
-flip -u /etc/ripple/validators.txt
+sudo chmod +x /etc/init.d/rippled2
+sudo flip -u /etc/init.d/rippled
+sudo flip -u /etc/init.d/rippled2
+sudo flip -u /etc/ripple/validators.txt
 
 # start service
-cp rippled_$1.cfg /etc/ripple/rippled.cfg
-cp rippled_$1.cfg $HOME/.config/ripple/rippled.cfg
-flip -u /etc/ripple/rippled.cfg
+sudo cp rippled_$1.cfg /etc/ripple/rippled.cfg
+sudo cp rippled_$1.cfg $HOME/.config/ripple/rippled.cfg
+sudo flip -u /etc/ripple/rippled.cfg
 sudo service rippled start
