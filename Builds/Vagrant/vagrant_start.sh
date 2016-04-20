2c3d128e511832f8113cd8eff0b918deab57a024
Rooms Guy <guy.rooms@cegeka.be>
2016-04-20 09:58:41 +0200

    first working version of rippled in vagrant, still to be cleaned up

--- Builds/Vagrant/vagrant_start.sh
+++ Builds/Vagrant/vagrant_start.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 vagrant ssh one -c "sudo service rippled2 start"
 vagrant ssh two -c "sudo service rippled2 start"
-vagrant ssh two -c "sudo service rippled2 start"
+vagrant ssh three -c "sudo service rippled2 start"
 
